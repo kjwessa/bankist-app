0ddcf542a4c546b01261e83ac23c9631614545e7
Kevin Wessa <kjwessa@gmail.com>
2023-04-11 09:47:51 -0500

    feat: add smooth scrolling to all nav links using event delegation

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,44 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
+//* Variables
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
+btnScrollTo.addEventListener("click", function (evt) {
+  const s1coordinates = section1.getBoundingClientRect();
+  window.scrollIntoView({ behavior: "smooth" });
+});
+
+//* Add smooth scrolling using event delegation
+document.querySelector(".nav__links").addEventListener("click", function (evt) {
+  // prevent default behavior of anchor link
+  evt.preventDefault();
+  // Matching strategy
+  if (evt.target.classList.contains("nav__link")) {
+    // Get the href attribute of the clicked link
+    const id = evt.target.getAttribute("href");
+    // Scroll to the section with the id of the clicked link
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//TODO Delete the code below after testing
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (evt) {
+//     // Prevent default behavior of anchor link
+//     evt.preventDefault();
+//     // Get the href attribute of the clicked link
+//     const id = this.getAttribute("href");
+//     // Scroll to the section with the id of the clicked link
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+//* Modal Window
 const openModal = function (evt) {
   evt.preventDefault();
   modal.classList.remove("hidden");
@@ -43,11 +74,29 @@ document.querySelector(".btn--close-cookie").addEventListener("click", function 
   message.remove();
 });
 
-//* Add smooth scrolling
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+// const randomINT = function (min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
 
-btnScrollTo.addEventListener("click", function (evt) {
-  const s1coordinates = section1.getBoundingClientRect();
-  window.scrollIntoView({ behavior: "smooth" });
-});
+// const randomColor = function () {
+//   return `rgb(${randomINT(0, 255)}, ${randomINT(0, 255)}, ${randomINT(0, 255)})`;
+// };
+
+// console.log(randomColor());
+
+// //* Add event listener to nav links
+// document.querySelector(".nav__link").addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   this.style.backgroundColor = randomColor();
+//   console.log(evt.target);
+// });
+
+// document.querySelector(".nav__links").addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector(".nav").addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   this.style.backgroundColor = randomColor();
+// });
