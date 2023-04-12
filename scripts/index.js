2105b580552c6dd6d00aeb82bfe115ad4f4d0fe8
Kevin Wessa <kjwessa@gmail.com>
2023-04-12 08:21:09 -0500

    feat: allow users to switch between tabs and indicate the active tab in the UI

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,9 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 btnScrollTo.addEventListener("click", function (evt) {
   const s1coordinates = section1.getBoundingClientRect();
@@ -26,17 +29,27 @@ document.querySelector(".nav__links").addEventListener("click", function (evt) {
   }
 });
 
-//TODO Delete the code below after testing
-// document.querySelectorAll(".nav__link").forEach(function (el) {
-//   el.addEventListener("click", function (evt) {
-//     // Prevent default behavior of anchor link
-//     evt.preventDefault();
-//     // Get the href attribute of the clicked link
-//     const id = this.getAttribute("href");
-//     // Scroll to the section with the id of the clicked link
-//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-//   });
-// });
+//* Tabbed Content Component
+tabsContainer.addEventListener("click", function (evt) {
+  // Get the clicked tab element
+  const clicked = evt.target.closest(".operations__tab");
+  // Prevent the function from running if the clicked element is not a tab
+  if (!clicked) return;
+
+  // Remove the active class from all tabs
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  // Remove the active class from all content areas
+  tabsContent.forEach((content) => {
+    content.classList.remove("operations__content--active");
+  });
+
+  // Add the active class to the clicked tab
+  clicked.classList.add("operations__tab--active");
+  // Activate the content area for the related tab
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
 
 //* Modal Window
 const openModal = function (evt) {
@@ -74,6 +87,19 @@ document.querySelector(".btn--close-cookie").addEventListener("click", function 
   message.remove();
 });
 
+//TODO Delete the code below after testing
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (evt) {
+//     // Prevent default behavior of anchor link
+//     evt.preventDefault();
+//     // Get the href attribute of the clicked link
+//     const id = this.getAttribute("href");
+//     // Scroll to the section with the id of the clicked link
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+//TODO Delete the code below after testing
 // const randomINT = function (min, max) {
 //   return Math.floor(Math.random() * (max - min + 1) + min);
 // };
@@ -100,3 +126,9 @@ document.querySelector(".btn--close-cookie").addEventListener("click", function 
 //   evt.preventDefault();
 //   this.style.backgroundColor = randomColor();
 // });
+
+// Going down the DOM tree
+// const h1 = document.querySelector("h1");
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.children);
+// h1.firstElementChild.style.color = "white";
